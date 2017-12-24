72a5f4d497a364b34e653d3824ae0a68a0b8f16b
Rizart Dokollari <r.dokollari@gmail.com>
2017-12-24 10:55:29 +0000

    Assert http client uses correct base uri.

---
+++ tests/unit/UnitTestCase.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author  Rizart Dokollari <r.dokollari@gmail.com>
+ * @since   12/24/17
+ */
+
+namespace Tests\unit;
+
+use Tests\TestCase;
+
+abstract class UnitTestCase extends TestCase
+{
+
+}
